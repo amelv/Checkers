f2b56d3eaa93a27155282e74e00d7adb27ec17c0
amelv <alex.melvin99@gmail.com>
2016-05-04 08:13:45 -0400

    Board

--- Checkers Game/src/Board.java
+++ Checkers Game/src/Board.java
@@ -1,8 +1,18 @@
 
 public class Board {
+	public final String EMPTY = "0";
+	public final String PEICE = "P";
+	public final String KING = "K";
+	
 	public String[][] board;
+	private final int LENGTH = 8;
+	private final int WIDTH = 8;
 	
 	public Board () {
-		
+		board = new String[8][8];
+		for (int r = 0; r < 3; r++) {
+			for (int c = 0; c < 8; c++) {
+			}
+		}
 	}
 }

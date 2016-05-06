5c013f54a4e7fb9acdb5a790f38692587b5cf801
amelv <amelv@amelv-desktop>
2016-05-05 20:53:02 -0400

    Makes array of pieces representing the checkers board

--- Checkers Game/src/Board.java
+++ Checkers Game/src/Board.java
@@ -10,9 +10,36 @@ public class Board {
 	
 	public Board () {
 		board = new String[8][8];
-		for (int r = 0; r < 3; r++) {
+		for (int r = 0; r < 8; r++) {
 			for (int c = 0; c < 8; c++) {
+				if (r < 3 || r > 4){
+					if (r % 2 == 0 && c % 2 == 1)
+						board[r][c] = PEICE;
+					else if (r % 2 == 1 && c % 2 == 0)
+						board[r][c] = PEICE;
+					else
+						board[r][c] = EMPTY;
+				} else {
+					board[r][c] = EMPTY;
+				}
+		
 			}
 		}
+	}
+	
+	public String peiceAt(int row, int col) {
+		return board[row][col];
+	}
+	
+	
+	public String toString() {
+		String boardString = "";
+		for (int r = 0; r < 8; r++) {
+			for (int c = 0; c < 8; c++) {
+				boardString += board[r][c];
+			}
+			boardString += "\n";
+		}
+		return boardString;
 	}
 }
---
+++ Checkers Game/src/BoardTest.java
@@ -0,0 +1,11 @@
+
+public class BoardTest {
+
+	public static void main(String[] args) {
+		
+		Board b1 = new Board();
+		
+		System.out.print(b1);
+	}
+
+}
